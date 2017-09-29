7927008c89579f140391dbb99cb8f56ae895e0fa
Sean Berry <seanberry@Seans-MacBook-Pro.local>
2017-09-29 11:00:39 -0500

    cover up eyes in a big image

--- selfieapp/FaceViewController.swift
+++ selfieapp/FaceViewController.swift
@@ -45,34 +45,53 @@ class FaceViewController: UIViewController {
         let opts = [CIDetectorImageOrientation : 2]
         let features = detector.features(in: image, options: opts)
         
+        for feature in features {
+            if let face = feature as? CIFaceFeature {
+                if face.hasLeftEyePosition, face.hasRightEyePosition {
+                    let width = 140
+                    let height = 60
+                    var leftEyeBounds = CGRect(x: 0, y: 0, width: width, height: height)
+                    leftEyeBounds = leftEyeBounds.setCenter(face.leftEyePosition)
+                    
+                    let redBox = UIView(frame: translateRect(leftEyeBounds, image: image))
+                    redBox.backgroundColor = UIColor.red
+                    self.imageView.addSubview(redBox)
+                    
+                    var rightEyeBounds = CGRect(x: 0, y: 0, width: width, height: height)
+                    rightEyeBounds = rightEyeBounds.setCenter(face.rightEyePosition)
+                    
+                    let blueBox = UIView(frame: translateRect(rightEyeBounds, image: image))
+                    blueBox.backgroundColor = UIColor.blue
+                    self.imageView.addSubview(blueBox)
+                }
+            }
+        }
+    }
+    
+    func translateRect(_ rect: CGRect, image: CIImage) -> CGRect {
         let ciImageSize = image.extent.size
         print("image extent:\(image.extent.size) - imageViewSize:\(imageView.bounds.size)")
         var transform = CGAffineTransform(scaleX: 1, y: -1)
         transform = transform.translatedBy(x: 0, y: -ciImageSize.height)
         
-        for feature in features {
-            if let face = feature as? CIFaceFeature {
-                if face.hasLeftEyePosition, face.hasRightEyePosition {
-                    
-                    // Apply the transform to convert the coordinates
-                    var faceViewBounds = face.bounds.applying(transform)
-                    /*
-                    // Calculate the actual position and size of the rectangle in the image view
-                    let viewSize = self.imageView.bounds.size
-                    let scale = min(viewSize.width / ciImageSize.width,
-                                    viewSize.height / ciImageSize.height)
-                    let offsetX = (viewSize.width - ciImageSize.width * scale) / 2
-                    let offsetY = (viewSize.height - ciImageSize.height * scale) / 2
-                    
-                    faceViewBounds = faceViewBounds.applying(CGAffineTransform(scaleX: scale, y: scale))
-                    faceViewBounds.origin.x += offsetX
-                    faceViewBounds.origin.y += offsetY*/
-                    
-                    let blackBox = UIView(frame: faceViewBounds)
-                    blackBox.backgroundColor = UIColor.red
-                    self.imageView.addSubview(blackBox)
-                }
-            }
-        }
+        var finalRect = rect.applying(transform)
+        
+        // Calculate the actual position and size of the rectangle in the image view
+        let viewSize = self.imageView.bounds.size
+        let scale = min(viewSize.width / ciImageSize.width,
+                        viewSize.height / ciImageSize.height)
+        let offsetX = (viewSize.width - ciImageSize.width * scale) / 2
+        let offsetY = (viewSize.height - ciImageSize.height * scale) / 2
+        
+        finalRect = finalRect.applying(CGAffineTransform(scaleX: scale, y: scale))
+        finalRect.origin.x += offsetX
+        finalRect.origin.y += offsetY
+        return finalRect
+    }
+}
+
+extension CGRect {
+    func setCenter(_ center: CGPoint) -> CGRect {
+        return CGRect(x: center.x - size.width/2, y: center.y + size.height/2, width: size.width, height: size.height)
     }
 }
